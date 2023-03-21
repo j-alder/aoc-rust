4223d62bb9bcaa5cd4162d5023b1aff986be1a89
j-alder <jonathon.alder@gmail.com>
2023-03-20 22:05:18 -0400

    year/day args; get soln

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod y2015;
+
+use std::env;
+use y2015::get_solution_for_day;
+
+fn get_solution_for_year_and_day(year: &str, day: &str) -> String {
+    match year {
+        "2015"=>get_solution_for_day(day),
+        _=>String::from("No solution found")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let year = &args[1];
+    let day = &args[2];
+
+    let soln = get_solution_for_year_and_day(year, day);
+    println!("{soln}");
 }
