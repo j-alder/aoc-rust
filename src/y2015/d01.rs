e996b52d54a347ecc08a8d28941c834878e5b0a2
j-alder <jonathon.alder@gmail.com>
2023-03-21 20:59:08 -0400

    reorganization; 2015 day 2

--- src/y2015/d01.rs
+++ src/y2015/d01.rs
@@ -1,4 +1,29 @@
-# Day 1: Not Quite Lisp
+use std::fs;
+use crate::util::format_soln_string;
+
+pub fn soln() -> String {
+    let input = fs::read_to_string("src/y2015/input/d01.txt")
+        .unwrap();
+
+    let mut f = 0;
+    let mut b: usize = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        f += match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0
+        };
+        if b == 0 && f == -1 {
+            b = i + 1;
+        }
+    }
+
+    return format_soln_string(f.to_string(), b.to_string())
+}
+
+/*
+--- Day 1: Not Quite Lisp ---
 
 Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
 
@@ -13,22 +38,23 @@ An opening parenthesis, (, means he should go up one floor, and a closing parent
 The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
 
 For example:
-```
+
     (()) and ()() both result in floor 0.
     ((( and (()(()( both result in floor 3.
     ))((((( also results in floor 3.
     ()) and ))( both result in floor -1 (the first basement level).
     ))) and )())()) both result in floor -3.
-```
-**To what floor do the instructions take Santa?**
 
-# Part Two
+To what floor do the instructions take Santa?
+
+--- Part Two ---
 
 Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
 
 For example:
-```
+
     ) causes him to enter the basement at character position 1.
     ()()) causes him to enter the basement at character position 5.
-```
-**What is the position of the character that causes Santa to first enter the basement?**
+
+What is the position of the character that causes Santa to first enter the basement?
+ */
--- src/y2015/mod.rs
+++ src/y2015/mod.rs
@@ -1,10 +1,13 @@
 pub mod d01;
+pub mod d02;
 
 use d01::soln as soln1;
+use d02::soln as soln2;
 
 pub fn get_solution_for_day(day: &str) -> String {
     match day {
         "1"=>soln1(),
+        "2"=>soln2(),
         _=>String::from("No solution found")
     }
 }
