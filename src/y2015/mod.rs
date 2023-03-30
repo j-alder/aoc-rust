a261b8f67b77c7163f5ddd272ad55b2b91b88254
Jonathon Alder <jonathon.alder@missionlane.com>
2023-03-30 10:58:45 -0400

    2015 day 4

--- src/y2015/mod.rs
+++ src/y2015/mod.rs
@@ -1,16 +1,19 @@
 pub mod d01;
 pub mod d02;
 pub mod d03;
+pub mod d04;
 
 use d01::soln as soln1;
 use d02::soln as soln2;
 use d03::soln as soln3;
+use d04::soln as soln4;
 
 pub fn get_solution_for_day(day: &str) -> String {
     match day {
         "1"=>soln1(),
         "2"=>soln2(),
         "3"=>soln3(),
+        "4"=>soln4(),
         _=>String::from("No solution found")
     }
 }
