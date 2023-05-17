4a2b82ae863d76c0e51ff73ad34134a3e9257888
j-alder <jonathon.alder@gmail.com>
2023-05-16 21:37:36 -0400

    2022 day 3 part 2 dirty

--- src/y2022/d03.rs
+++ src/y2022/d03.rs
@@ -1,31 +1,49 @@
-use std::fs;
+use std::{fs, mem::take};
 use crate::util::format_soln_string;
 
-fn get_priority(c: char) -> u32 {
+fn get_priority(c: char) -> i32 {
     if c.is_uppercase() {
-        c as u32 - 38
+        c as i32 - 38
     } else {
-        c as u32 - 96
+        c as i32 - 96
     }
 }
 
-pub fn soln() -> String {
-    let input = fs::read_to_string("src/y2022/input/d03.txt")
-        .unwrap();
-
-    let mut sum = 0;
-
-    for rucksack in input.split('\n').into_iter() {
-        let comps = rucksack.split_at(rucksack.len() / 2);
-        for item in comps.0.chars() {
-            if comps.1.contains(item) {
-                sum += get_priority(item);
-                break;
-            }
+fn find_match_priority(c: (&str, &str)) -> i32 {
+    for item in c.0.chars() {
+        if c.1.contains(item) {
+            return get_priority(item);
         }
     }
+    return 0;
+}
 
-    return format_soln_string(sum.to_string(), "Incomplete".to_string())
+fn find_group_priority(c: (&String, &String, &String)) -> i32 {
+    for item in c.0.chars() {
+        if c.1.contains(item) && c.2.contains(item) {
+            return get_priority(item);
+        }
+    }
+    return 0;
+}
+
+pub fn soln() -> String {
+    let input = fs::read_to_string("src/y2022/input/d03.txt").unwrap();
+
+    let part_one_sum = input.split('\n').fold(0, |a: i32, r| {
+        let comps = r.split_at(r.len() / 2);
+        a + find_match_priority(comps)
+    });
+
+    let rucksacks: Vec<String> = input.split('\n').map(|st| {String::from(st)}).collect();
+    let mut part_two_sum = 0;
+
+    for x in (0..rucksacks.len()-1).step_by(3) {
+        let sacks = (&rucksacks[x], &rucksacks[x+1], &rucksacks[x+2]);
+        part_two_sum += find_group_priority(sacks);
+    }
+
+    return format_soln_string(part_one_sum.to_string(), part_two_sum.to_string())
 }
 
 /*
